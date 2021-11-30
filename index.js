353b36dd5236628b46739f402e7c1b5ed0d182cf
Ido Strauss <strauss02@hotmail.co.il>
2021-11-30 17:22:11 +0200

    basic setup

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-/* write the code to run app.js here */
+/* ===== Dependencies ===== */
+const express = require('express');
+const app = express()
+const Router = express.Router()
+/* ===== Constants ===== */
+// Passwords cannot be stored as plain-text - only as hash+salt(10!)
+const PORT = 8080
+const USERS = [{ email: "admin@email.com",
+                  name: "admin",
+  password: "**hashed password**",
+   isAdmin: true }.
+]
+const INFORMATION = []
+const REFRESHTOKENS = []
+/* ===== ============ ===== */
+
+
+app.get('/', (req,res)=> {
+  res.send('hello world')
+})
+
+app.listen(PORT, ()=> console.log(`Listening on ${PORT}`))
