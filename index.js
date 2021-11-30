604b0b66db822cb8f527d53349ff3401dda00b44
Ido Strauss <strauss02@hotmail.co.il>
2021-11-30 18:04:05 +0200

    add hash function

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 /* ===== Dependencies ===== */
-const express = require('express');
+const express = require('express')
 const app = express()
 const Router = express.Router()
+const bcrypt = require('bcrypt')
 /* ===== Constants ===== */
-// Passwords cannot be stored as plain-text - only as hash+salt(10!)
 const PORT = 8080
-const USERS = [{ email: "admin@email.com",
-                  name: "admin",
-  password: "**hashed password**",
-   isAdmin: true }.
+// Passwords cannot be stored as plain-text - only as hash+salt(10!)
+const USERS = [
+  {
+    email: 'admin@email.com',
+    name: 'admin',
+    password: '**hashed password**',
+    isAdmin: true,
+  },
 ]
 const INFORMATION = []
 const REFRESHTOKENS = []
+const HASHES = {}
 /* ===== ============ ===== */
 
+async function generatePasswordHash(password) {
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+  HASHES[hashedPassword] = salt
+  console.log(hashedPassword)
+  return hashedPassword
+}
 
-app.get('/', (req,res)=> {
+app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-app.listen(PORT, ()=> console.log(`Listening on ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
