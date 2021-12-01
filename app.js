81e70f34dbd752116f2edf19f26f461955974faf
Ido Strauss <strauss02@hotmail.co.il>
2021-12-01 14:33:45 +0200

    make register endpoint work

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-/* write your server code here */
 /* ===== Dependencies ===== */
+
 const express = require('express')
 const app = express()
-const Router = express.Router()
 const bcrypt = require('bcrypt')
-const { json } = require('express')
 const morgan = require('morgan')
+
 /* ===== Constants ===== */
+
 const PORT = 8080
+
 // Passwords cannot be stored as plain-text - only as hash+salt(10!)
 const USERS = [
   {
@@ -17,13 +18,17 @@ const USERS = [
     isAdmin: true,
   },
 ]
+
 const INFORMATION = []
+
 const REFRESHTOKENS = []
+
 // {hashedPassword : salt}
 const HASHES = {
   $2b$10$tgqItOt6gzm9zJ3b1nd5vOarzktWlBYgorHeaIlcdVvmJM5UFI2km:
     '$2b$10$tgqItOt6gzm9zJ3b1nd5vO',
 }
+
 /* ===== Hash Function =====*/
 
 async function generatePasswordHash(password) {
@@ -36,20 +41,20 @@ async function generatePasswordHash(password) {
 /* ===== ============ ===== */
 
 app.use(morgan('tiny'))
-app.use(express.json)
+app.use(express.json())
 
 app.post('/users/register', (req, res) => {
-  if (INFORMATION[req.body.email]) {
+  console.log(req.body.email)
+  console.log(INFORMATION)
+
+  if (INFORMATION.some((userInfo) => userInfo.email === req.body.email)) {
     res.status(409).send('user already exists')
     res.end()
     return
   }
-  INFORMATION.email = [req.body.email]
-  INFORMATION.info = `${req.body.user} info`
+  INFORMATION.push({ email: req.body.email, info: `${req.body.user} info` })
   res.status(201).send('Register Success')
   res.end()
 })
-
-app.post('/users/login', (req, res) => {})
 
 module.exports = app
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const express = require('express')
 const app = require('./app.js')
-const morgan = require('morgan')
 const PORT = 8080
 
-app.use(morgan('tiny'))
-
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`)
+})
